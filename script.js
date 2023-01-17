233a35668912ba8e8d590dde71893ac58e5f4e45
marleyschneiderr <marley33@uw.edu>
2023-01-16 22:30:39 -0800

    Adding rest of arrays with information

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ var CharLength = 8;
 var selectArray = [];
 
 
-var numbersArray = [];
-var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '{', '}', '[', ']', '|', '/', '\', ';', ':', '<', '>', '?', '.', ',', ''' ];
-var upperCaseArray = [];
-var lowerCaseArray = [];
+var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '{', '}', '[', ']', '|', '/', ';', ':', '<', '>', '?', '.'];
+var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 
